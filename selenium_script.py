5a6ddefe3087ae89a2ca0495ed034ae76dcee306
ShivamGupta82 <114674655+ShivamGupta82@users.noreply.github.com>
2024-04-10 22:24:35 +0530

    Update selenium_script.py

--- selenium_script.py
+++ selenium_script.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
-url = "view/index.html"  
+url = "view/index.html" 
 
 # Configuring Chrome WebDriver options
 options = webdriver.ChromeOptions()
@@ -11,26 +11,23 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
-driver = None
-try:
-    # Initialize Chrome WebDriver
-    driver = webdriver.Chrome(options=options, executable_path='chromedriver')
+# Creating a WebDriver instance
+driver = webdriver.Chrome(options=options)
 
-    # Open the web page
-    driver.get(url)
+# Open the web page
+driver.get(url)
 
-    # Get the page source
-    page_source = driver.page_source
+# Get the page source
+page_source = driver.page_source
 
-    # Use BeautifulSoup to parse the HTML content
-    soup = BeautifulSoup(page_source, 'html.parser')
+# Use BeautifulSoup to parse the HTML content
+soup = BeautifulSoup(page_source, 'html.parser')
 
-    # Check if the HTML structure is appropriate
-    if soup.find('html') and soup.find('head') and soup.find('body'):
-        print("Test Passed: HTML file has appropriate format.")
-    else:
-        print("Test Failed: HTML file does not have appropriate format.")
-finally:
-    # Close the WebDriver
-    if driver:
-        driver.quit()
+# Check if the HTML structure is appropriate
+if soup.find('html') and soup.find('head') and soup.find('body'):
+    print("Test Passed: HTML file has appropriate format.")
+else:
+    print("Test Failed: HTML file does not have appropriate format.")
+
+# Close the WebDriver
+driver.quit()
